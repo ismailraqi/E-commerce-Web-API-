e669bc7c9906590d9217774e1d118d1cbf06c4fa
walrusdev <raqiking@gmail.com>
2020-01-24 12:15:26 +0000

    Add project files.

---
+++ WatchStore/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WatchStore.Startup))]
+
+namespace WatchStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
